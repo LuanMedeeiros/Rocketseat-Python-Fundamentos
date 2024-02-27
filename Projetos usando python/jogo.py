ca61d60a85ce7d9da339d8be7062ff02dc7da2f3
LuanMedeeiros <luan.medeeiros25@gmail.com>
2024-02-27 16:24:15 -0300

    add commit changes

--- Projetos usando python/jogo.py
+++ Projetos usando python/jogo.py
@@ -20,6 +20,16 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNivel: {self.get_nivel()}"
     
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida <= 0:
+            self.__vida = 0
+
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"\nO {self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
         super().__init__(nome, vida, nivel)
@@ -57,8 +67,18 @@ class Jogo:
             print(self.inimigo.exibir_detalhes())
 
             input("\nPressione Enter para atacar...")
-            escolha = input("1 - Ataque Normal\n2- Ataque Especial\nEscolha: ")
+            escolha = input("Escolha (1 - Ataque Normal, 2- Ataque Especial): ")
+
+            if escolha == '1':
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida. Escolha novamente.")
         
+        if self.heroi.get_vida() > 0:
+            print("\nParabéns você venceu a batalha!")
+        else:
+            print("\nVocê foi derrotado.")
+
 # Criar intância do jogo e iniciar batalha
 jogo = Jogo()
 jogo.iniciar_batalha()
